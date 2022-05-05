09a73907989e2c2ae37c2233bccee19b8560f621
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2022-05-05 21:36:49 +0100

    Working Pterosync Script The script works at a basic level and creates a folder for each server defined in the array, where each server has its own set of incremental backups.

---
+++ pterosync.sh
@@ -0,0 +1,62 @@
+# Pterosync 1.0 by chiralpenguin
+# Automated backup script for Pterodactyl servers using rsync iterative backups
+# This script is intended to automate backups from a remote server via SSH by
+# running the script from cron on the backup server.
+
+# Define connection information - username, password, address and SSH port of 
+# remote host
+# If password="", key authentication will be used. You SHOULD avoid password-
+# based authentication
+readonly ADDRESS=127.0.0.1
+readonly USERNAME="admin"
+readonly PASSWD=""
+readonly PORT=22
+
+# Define paths for backup location on the local machine and pterodactyl server
+# location on the remote machine
+# Credit to Egidio Docile (https://linuxconfig.org/how-to-create-incremental-backups-using-rsync-on-linux)
+# for implementation of hard links with rsync
+readonly PTERO_DIR="/var/lib/pterodactyl/volumes"
+readonly BACKUP_DIR="/home/backupuser/Backups"
+
+declare -A servers
+
+# Define servers with the UUID of the docker container as the ID and the server
+# name as the value in the following format
+# servers[UUID]="Server Name"
+servers[c5a99fd1-b655-47e0-8648-66eb62e73a9e]="My Server"
+servers[5a852865-aacf-4225-be26-f09209152c6d]="Another Server"
+
+# Define if the backup operation will delete files locally which do not exist on
+# the remove machine anymore (Y/N)
+delete="N"
+
+for id in ${!servers[@]}
+do
+    echo "Backing up: ${servers[$id]} (UUID: ${id})"
+    declare SOURCE_DIR"=${PTERO_DIR}/${id}"
+    declare DATETIME="$(date '+%Y-%m-%d_%H:%M:%S')"
+    declare BACKUP_PATH="${BACKUP_DIR}/${servers[$id]}/${DATETIME}"
+    declare LATEST_LINK="${BACKUP_PATH}/../latest"
+
+    mkdir -p "${BACKUP_PATH}"
+
+    if [ "${delete}" == "Y" ]
+    then
+        rsync -av --delete --progress \
+          -e 'ssh -p ${PORT}' \
+          "${USERNAME}@${ADDRESS}:${SOURCE_DIR}/" \
+          --link-dest "${LATEST_LINK}" \
+          "${BACKUP_PATH}"
+          
+    else
+        rsync -av --progress \
+          -e "ssh -p ${PORT}" \
+          "${USERNAME}@${ADDRESS}:${SOURCE_DIR}/" \
+          --link-dest "${LATEST_LINK}" \
+          "${BACKUP_PATH}"
+    fi
+
+    rm -rf "${LATEST_LINK}"
+    ln -s "${BACKUP_PATH}" "${LATEST_LINK}"
+done
